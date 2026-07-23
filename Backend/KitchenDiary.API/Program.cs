be0ac45621d753fc87932d191eb651aeb7b1581c
ayeshaiqbal09 <sarajames0015@gmail.com>
2026-07-23 12:24:19 +0530

    Configured docker file

--- Backend/KitchenDiary.API/Program.cs
+++ Backend/KitchenDiary.API/Program.cs
@@ -10,7 +10,11 @@ using Microsoft.IdentityModel.Tokens;
 //using Microsoft.OpenApi;
 using System.Text;
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.Sources.Clear();
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .AddEnvironmentVariables();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
